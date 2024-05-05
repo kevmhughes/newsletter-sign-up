d44d76b4d16e6b53d63d9a9c4d3d9fe32a97f59b
kevmhughes <kevhughes24@hotmail.com>
2024-05-05 03:33:27 +0200

    Make Pretiier

--- src/Card.jsx
+++ src/Card.jsx
@@ -24,67 +24,75 @@ const Card = () => {
   return (
     <div className="main-outer-container">
       <div className="signup-container">
-      <div className="mobile-view-container">
-      <div>
-          <img className="mobile-image" src={MobileImage} alt="colourful image on the sign-up page" />
-        </div>
-        <div className="stay-updated-container">
-          <h1>Stay updated!</h1>
-          <p className="stay-updated-text">
-            Join 60,000+ product managers receiving monthly updates on:
-          </p>
-          <div className="check-points-container">
-            <div className="check-points">
-              <img
-                className="success-tick"
-                src={Success}
-                alt="tick for success"
-              />
-              <div>Product discovery and building what matters</div>
-            </div>
-            <div className="check-points">
-              <img
-                className="success-tick"
-                src={Success}
-                alt="tick for success"
-              />
-              <div>Measuring to ensure updates are a success</div>
-            </div>
-            <div className="check-points">
-              <img
-                className="success-tick"
-                src={Success}
-                alt="tick for success"
-              />
-              <div>And much more!</div>
-            </div>
+        <div className="mobile-view-container">
+          <div>
+            <img
+              className="mobile-image"
+              src={MobileImage}
+              alt="colourful image on the sign-up page"
+            />
           </div>
-          <form
-            className="form-container"
-            onSubmit={handleSubmit}
-            method="post"
-          >
-            <label className="email-label" htmlFor="email-subscription">
-              Email address{" "}
-            </label>
-            <input
-              className="email-input"
-              type="text"
-              id="email"
-              pattern="^[^\W_]+\w*(?:[.-]\w*)*[^\W_]+@[^\W_]+(?:[.-]?\w*[^\W_]+)*(?:\.[^\W_]{2,})$"
-              placeholder="email@company.com"
-              value={emailsend}
-              onChange={handleChange}
-              required
-            ></input>
-            <button id="btn" className="subscribe-button" type="submit">
-              Subscribe to monthly newsletter
-            </button>
-          </form>
-        </div>
+          <div className="stay-updated-container">
+            <h1>Stay updated!</h1>
+            <p className="stay-updated-text">
+              Join 60,000+ product managers receiving monthly updates on:
+            </p>
+            <div className="check-points-container">
+              <div className="check-points">
+                <img
+                  className="success-tick"
+                  src={Success}
+                  alt="tick for success"
+                />
+                <div>Product discovery and building what matters</div>
+              </div>
+              <div className="check-points">
+                <img
+                  className="success-tick"
+                  src={Success}
+                  alt="tick for success"
+                />
+                <div>Measuring to ensure updates are a success</div>
+              </div>
+              <div className="check-points">
+                <img
+                  className="success-tick"
+                  src={Success}
+                  alt="tick for success"
+                />
+                <div>And much more!</div>
+              </div>
+            </div>
+            <form
+              className="form-container"
+              onSubmit={handleSubmit}
+              method="post"
+            >
+              <label className="email-label" htmlFor="email-subscription">
+                Email address{" "}
+              </label>
+              <input
+                className="email-input"
+                type="text"
+                id="email"
+                pattern="^[^\W_]+\w*(?:[.-]\w*)*[^\W_]+@[^\W_]+(?:[.-]?\w*[^\W_]+)*(?:\.[^\W_]{2,})$"
+                placeholder="email@company.com"
+                value={emailsend}
+                onChange={handleChange}
+                required
+              ></input>
+              <button id="btn" className="subscribe-button" type="submit">
+                Subscribe to monthly newsletter
+              </button>
+            </form>
+          </div>
         </div>
         <div>
-          <img className="main-image" src={MainImage} alt="colourful image on the sign-up page" />
+          <img
+            className="main-image"
+            src={MainImage}
+            alt="colourful image on the sign-up page"
+          />
         </div>
       </div>
     </div>
--- src/CardTwo.jsx
+++ src/CardTwo.jsx
@@ -1,38 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import Success from "./assets/images/icon-success.svg";
-import { useNavigate } from 'react-router-dom';
-import { MyContext } from "./App.jsx"
+import { useNavigate } from "react-router-dom";
+import { MyContext } from "./App.jsx";
 
 const CardTwo = () => {
+  const { emailsend, setEmailsend } = useContext(MyContext);
 
-
-const {emailsend, setEmailsend} = useContext(MyContext);
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleClick = (e) => {
-    navigate("/")
-    setEmailsend("")
- } 
+    navigate("/");
+    setEmailsend("");
+  };
 
   return (
     <div>
-      <div className='success-container'>
-        <div className='inner-success-container'>
-        <img className="success-page-tick" src={Success} alt="a tick for success" />
-        <div className='thanks-text'>
-          Thanks for subscribing!
+      <div className="success-container">
+        <div className="inner-success-container">
+          <img
+            className="success-page-tick"
+            src={Success}
+            alt="a tick for success"
+          />
+          <div className="thanks-text">Thanks for subscribing!</div>
+          <div className="confirmation-message">
+            A confirmation message has been sent to <b>{emailsend}</b>. Please
+            open it and click the button inside to confirm your subscription.
+          </div>
         </div>
-        <div className='confirmation-message'>
-          A confirmation message has been sent to <b>{emailsend}</b>. Please open it and click the button inside to confirm your subscription.
-        </div>
-        </div>
-        <button className='dismiss-button' onClick={handleClick}>
+        <button className="dismiss-button" onClick={handleClick}>
           Dismiss message
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardTwo
+export default CardTwo;
